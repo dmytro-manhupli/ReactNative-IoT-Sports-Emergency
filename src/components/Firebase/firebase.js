3ebb1563dc4212e51cc6b425e0b130e5cda42dc9
dmytro-manhupli <dmytro.manhupli@gmail.com>
2025-01-09 16:14:08 +0800

    chore: security

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase'
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8NyWPjkQLV6duBnyAf-K9WE-UDbiaLOQ',
+  apiKey: '',
   authDomain: 'project-4-c79e1.firebaseapp.com',
   databaseURL: 'https://project-4-c79e1.firebaseio.com',
   projectId: 'project-4-c79e1',
